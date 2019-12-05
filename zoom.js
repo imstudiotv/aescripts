7760001681e53464e355c7e57aecd693a457d3dd
imstudiotv <58565274+imstudiotv@users.noreply.github.com>
2019-12-05 10:53:13 -0600

    Create zoom.js

---
+++ zoom.js
@@ -0,0 +1,8 @@
+/**
+* Zoom IN/OUT Capas After Effects
+* Script para usar en capas de after effects, sobre la propiedad escala
+**/
+vel=2;
+zoom=(time-inPoint)*vel;
+dimension=100;
+[dimension+zoom,dimension+zoom];
